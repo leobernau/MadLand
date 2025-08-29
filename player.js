dac655afbf7e8fcc3f5c284ba630fb23a7c99016
leobernau <leobernau4@gmail.com>
2025-08-29 19:09:07 +0200

    Update player.js

--- player.js
+++ player.js
@@ -1,28 +1,23 @@
-class Player {
-    constructor(x, y){
-        this.x = x;
-        this.y = y;
-        this.width = 50;
-        this.height = 50;
-        this.health = 100;
-        this.maxHealth = 100;
-        this.speed = 5;
-        this.dashSpeed = 20;
-        this.dashCooldown = 0;
-        this.lastDir = {x:1, y:0};
-        this.upgrades = [];
-        this.xp = 0;
-        this.level = 1;
-    }
+let player = {
+    x: window.innerWidth/2,
+    y: window.innerHeight/2,
+    width:50,
+    height:50,
+    health:100,
+    maxHealth:100,
+    speed:5,
+    dashSpeed:20,
+    dashCooldown:0,
+    lastDir:{x:1,y:0},
+    upgrades:[],
+    xp:0,
+    level:1,
+    crystals:0,
+    img:null
+};
+player.img = new Image();
+player.img.src = 'assets/player.png';
 
-    draw(ctx){
-        // Spieler-Sprite
-        let img = new Image();
-        img.src = 'images/player.png';
-        ctx.drawImage(img, this.x - this.width/2, this.y - this.height/2, this.width, this.height);
-
-        // Healthbar
-        ctx.fillStyle = 'red';
-        ctx.fillRect(this.x-25, this.y-35, 50*(this.health/this.maxHealth), 5);
-    }
-}
+let keys = {};
+document.addEventListener('keydown', e=>keys[e.key.toLowerCase()]=true);
+document.addEventListener('keyup', e=>keys[e.key.toLowerCase()]=false);
